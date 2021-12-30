41c38c9eed58fc9e444ab8d2a4986ac1be3b494f
Holly Rayson <holly.rayson1@gmail.com>
2021-12-30 17:11:33 +0100

    initial import

---
+++ attention_bias_analysis.R
@@ -0,0 +1,85 @@
+library("lme4")
+library("ggplot2")
+library("lsmeans")
+library("car")
+library("doBy")
+
+dropbox_dir='C:/Users/Holly Rayson/Dropbox/'
+# Read gaze data
+data<-read.csv(file=paste0(dropbox_dir,'/joint_attention/juvenile_monkeys/pref_gaze/data/year1/all_facial_gestures_gaze_summary.csv'),header=TRUE,sep=',')
+data<-read.csv(file=data_file,header=TRUE,sep=',')
+
+#Only consider trials where proportion onscreen is more than 0
+n_trials_pre<-nrow(data)
+data<- data[data$PropOnscreenAmbigOther>0,]
+n_trials_post<-nrow(data)
+print(paste0((n_trials_pre-n_trials_post),' trials removed where PropOnscreenAmbigOther==0'))
+
+
+## Threat and LPS bias
+# Remove outliers
+sds<-2.5
+print(paste0('Removing outliers >',sds,' deviations from the mean'))
+
+# within subject and condition
+data_filt<-data.frame()
+subjects<-unique(data$Subject)
+conditions<-unique(data$EmotionType)
+for (i in 1:length(subjects)) {
+  for (j in 1:length(conditions)) {
+    subj_data<-data[data$Subject==subjects[i] & data$EmotionType==conditions[j],]
+    mean_bias<-mean(subj_data$EmotionBiasOnscreenAmbigOther)
+    sd_bias<-sd(subj_data$EmotionBiasOnscreenAmbigOther)
+    outliers<-which(abs(subj_data$EmotionBiasOnscreenAmbigOther)>mean_bias+sds*sd_bias)
+    if (length(outliers)==0) {
+      subj_data_filt<-subj_data
+    } else{
+      subj_data_filt<-subj_data[-outliers, ]
+    }
+    data_filt<-rbind(data_filt, subj_data_filt)
+  }
+}
+noutliers<-nrow(data)-nrow(data_filt)
+print(paste0('Within subject and condition: ',noutliers,' trials removed'))
+
+#Load rank data
+rank_file<-paste0(dropbox_dir,'/joint_attention/juvenile_monkeys/rank/rank.csv')
+ranks<-read.csv(file=rank_file,header=TRUE,sep=',')
+ranks$Cscore_m<-scale(ranks$score_m)
+
+# Merge behavior and gaze data
+names(ranks)[names(ranks) == "subject"] <- "Subject"
+all_data<-merge(ranks,data_filt,by=c('Subject'))
+
+
+# Mixed model
+lmer_model <- lmer(EmotionBiasOnscreenAmbigOther ~ EmotionType*Group+Cscore_m+(1|Subject), data = all_data, control = lmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e6)))
+shapiro.test(resid(lmer_model))
+# Test for homoscedascity
+leveneTest(residuals(lmer_model) ~ all_data$EmotionType*all_data$Group)
+
+# Check coefficients
+print(lmer_model)
+
+results <- Anova(lmer_model, type = 3, test.statistic='F')
+print(results)
+
+print(emmeans(lmer_model,pairwise~Group, type='response'))
+print(emmeans(lmer_model,pairwise~EmotionType, type='response'))
+
+twow_pw1<-emmeans(lmer_model, pairwise~EmotionType|Group, adjust='tukey')
+print(summary(twow_pw1)$contrasts)
+
+twow_pw2<-lsmeans(lmer_model, pairwise~Group|EmotionType, adjust='tukey')
+print(summary(twow_pw2)$contrasts)
+
+summary_data_filt<-summaryBy(EmotionBiasOnscreenAmbigOther+PropEmotionalOnscreenAmbigOther+PropNeutralOnscreenAmbigOther~Subject+Group+EmotionType, data = data_filt, keep.names = TRUE)
+g<-ggplot(data_filt, aes(x=EmotionType, y=EmotionBiasOnscreenAmbigOther, group=Subject, color=Group)) + 
+  geom_point(aes(colour=Group), position=position_jitter(width=0.2))+
+  geom_point(data=summary_data_filt, aes(x=EmotionType, y=EmotionBiasOnscreenAmbigOther, colour=Group), size=3)+
+  geom_line(data=summary_data_filt, aes(x=EmotionType, y=EmotionBiasOnscreenAmbigOther, colour=Group, group=Subject), size=1)+
+  geom_boxplot(data=summary_data_filt, aes(x=EmotionType, y=EmotionBiasOnscreenAmbigOther, group=EmotionType), colour='black', outlier.size=0)+
+  scale_colour_brewer(palette='Set1')+
+  facet_grid(~Group)+ylab('Emotion Bias')+xlab('Emotion Type')+theme_bw()
+print(g)
+
---
+++ attention_bias_groom_analysis.R
@@ -0,0 +1,256 @@
+library("lme4")
+library("ggplot2")
+library("car")
+library("doBy")
+source('plot_fit.R')
+library('emmeans')
+
+
+dropbox_dir='C:/Users/Holly Rayson/Dropbox/'
+# Read gaze data
+gaze_data<-read.csv(file=paste0(dropbox_dir,'/joint_attention/juvenile_monkeys/pref_gaze/data/year1/all_facial_gestures_gaze_summary.csv'),header=TRUE,sep=',')
+
+sex_file<-paste0(dropbox_dir,'/joint_attention/juvenile_monkeys/juvenile_sex_ids.csv')
+sex_data<-read.csv(file=sex_file,header=TRUE,sep=',')
+sex_data$Sex<-as.factor(sex_data$Sex)
+
+#Only consider trials where proportion onscreen is more than 0
+n_trials_pre<-nrow(gaze_data)
+gaze_data<- gaze_data[gaze_data$PropOnscreenAmbigOther>0,]
+n_trials_post<-nrow(gaze_data)
+print(paste0((n_trials_pre-n_trials_post),' gaze trials removed with proportion onscreen/ambigOther is 0'))
+
+# Remove gaze outliers
+gaze_sds<-2.5
+gaze_data_filt<-data.frame()
+subjects<-unique(gaze_data$Subject)
+conditions<-unique(gaze_data$EmotionType)
+for (i in 1:length(subjects)) {
+  for (j in 1:length(conditions)) {
+    subj_data<-gaze_data[gaze_data$Subject==subjects[i] & gaze_data$EmotionType==conditions[j],]
+    mean_bias<-mean(subj_data$EmotionBiasOnscreenAmbigOther)
+    sd_bias<-sd(subj_data$EmotionBiasOnscreenAmbigOther)
+    outliers<-which(abs(subj_data$EmotionBiasOnscreenAmbigOther)>mean_bias+gaze_sds*sd_bias)
+    if (length(outliers)==0) {
+      subj_data_filt<-subj_data
+    } else{
+      subj_data_filt<-subj_data[-outliers, ]
+    }
+    gaze_data_filt<-rbind(gaze_data_filt, subj_data_filt)
+  }
+}
+noutliers<-nrow(gaze_data)-nrow(gaze_data_filt)
+print('')
+print(paste0('Within subject and condition: ',noutliers,' removed from gaze_data'))
+
+# Average over trials to get gaze bias
+summary_gaze_data<-summaryBy(EmotionBias+EmotionBiasLeftRight+EmotionBiasOnscreen+EmotionBiasOnscreenAmbigOther~Subject+EmotionType, data = gaze_data_filt, keep.names = TRUE)
+# Convert to wide to get gaze bias column for each expression type
+wide_summary_gaze_data<-reshape(summary_gaze_data, idvar = "Subject", timevar = "EmotionType", direction = "wide")
+wide_summary_gaze_data$CEmotionBiasOnscreenAmbigOther.threat<-scale(wide_summary_gaze_data$EmotionBiasOnscreenAmbigOther.threat)[,1]
+wide_summary_gaze_data$CEmotionBiasOnscreenAmbigOther.lps<-scale(wide_summary_gaze_data$EmotionBiasOnscreenAmbigOther.lps)[,1]
+
+#Load rank data
+rank_file<-paste0(dropbox_dir,'/joint_attention/juvenile_monkeys/rank/rank.csv')
+ranks<-read.csv(file=rank_file,header=TRUE,sep=',')
+# Merge behavior and gaze data
+names(ranks)[names(ranks) == "subject"] <- "Subject"
+ranks$Cscore_m<-scale(ranks$score_m)
+
+# Read observation data
+obs_data<-read.csv(file=paste0(dropbox_dir,'/joint_attention/juvenile_monkeys/all_group-ind_obs_summary.csv'),header=TRUE,sep=',')
+# Remove weird extra column
+obs_data = subset(obs_data, select = -c(X) )
+# Use only group observation
+obs_data<-obs_data[obs_data$ObsType=='group',]
+obs_data$TotalGroomIDInterruptedBoutDur<-obs_data$TotalGroomIDInterruptedDur/obs_data$TotalGroomIDInterruptedFreq
+obs_data$TotalGroomIDInterruptedBoutDur[is.nan(obs_data$TotalGroomIDInterruptedBoutDur)]<-0
+
+
+#####
+# GROOM DURATION
+#####
+# Remove outliers - within subject
+obs_sds<-2.5
+print(paste0('Removing outliers >',obs_sds,' deviations from the mean'))
+
+#### Total Groom
+# Remove outliers from behavioural data
+obs_data_filt<-data.frame()
+subjects<-unique(obs_data$Subject)
+for (i in 1:length(subjects)) {
+  subj_data<-obs_data[obs_data$Subject==subjects[i],]
+  mean_val<-mean(subj_data$TotalGroomIDInterruptedDur)
+  sd_val<-sd(subj_data$TotalGroomIDInterruptedDur)
+  outliers<-which(abs(subj_data$TotalGroomIDInterruptedDur)>mean_val+obs_sds*sd_val)
+  if (length(outliers)==0) {
+    subj_data_filt<-subj_data
+  } else{
+    subj_data_filt<-subj_data[-outliers, ]
+  }
+  obs_data_filt<-rbind(obs_data_filt, subj_data_filt)
+}
+noutliers<-nrow(obs_data)-nrow(obs_data_filt)
+print('')
+print(paste0('Within subject: ',noutliers,' removed from behavior'))
+
+# Merge behavior and gaze data
+all_data_filt<-merge(wide_summary_gaze_data,obs_data_filt,by=c('Subject'),all.x = TRUE, all.y = TRUE)
+all_data_filt<-merge(all_data_filt,ranks,by=c('Subject'),all.x = TRUE, all.y = TRUE)
+all_data_filt$Subject<-as.factor(all_data_filt$Subject)
+all_data_filt<-merge(all_data_filt,sex_data,by=c('Subject'))
+
+
+
+## THREAT - Total Groom
+# Just group observation data: Mixed model with group and threat bias as fixed effects
+print('Group')
+glmernb_group_model <- glmer.nb(TotalGroomIDInterruptedDur ~ Group*CEmotionBiasOnscreenAmbigOther.threat+Cscore_m+(1|Subject), data = all_data_filt, control = glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e6)))
+print(glmernb_group_model)
+
+glmer_results<-Anova(glmernb_group_model, type = 3)
+print(glmer_results)
+
+print(emmeans(glmernb_group_model,pairwise~Group, type='response'))
+print(emtrends(glmernb_group_model,~Group,var="Cscore_m", type='response'))
+
+pw<-emtrends(glmernb_group_model,~Group|Group,var="CEmotionBiasOnscreenAmbigOther.threat")
+print(summary(pw, infer = c(TRUE, TRUE)))
+
+
+summary_data_filt<-summaryBy(EmotionBiasOnscreenAmbigOther.threat+TotalGroomIDInterruptedDur~Subject+Group, data = all_data_filt, keep.names = TRUE)
+group_pred<-plot_fit(glmernb_group_model,focal_var = "CEmotionBiasOnscreenAmbigOther.threat",inter_var = c("Group"),RE = "Subject")
+group_pred$CEmotionBiasOnscreenAmbigOther.threat<-(group_pred$CEmotionBiasOnscreenAmbigOther.threat*sd(wide_summary_gaze_data$EmotionBiasOnscreenAmbigOther.threat))+mean(wide_summary_gaze_data$EmotionBiasOnscreenAmbigOther.threat)
+
+dev.new()
+g <- ggplot(all_data_filt)+
+ geom_point(aes(x=EmotionBiasOnscreenAmbigOther.threat, y=TotalGroomIDInterruptedDur, colour=Group), alpha = 0.2, position='jitter') +
+ geom_point(data=summary_data_filt, aes(x=EmotionBiasOnscreenAmbigOther.threat, y=TotalGroomIDInterruptedDur, colour=Group), size=3)+
+ geom_line(aes(x=CEmotionBiasOnscreenAmbigOther.threat, y=Pred, colour=Group), data=group_pred, size=1) +
+ geom_ribbon(aes(x=CEmotionBiasOnscreenAmbigOther.threat,y=Pred, fill=Group,ymin=LSE,ymax=USE, colour=Group), data=group_pred, alpha=0.5)+
+ scale_colour_brewer(palette='Set1')+theme_bw()
+print(g)
+
+
+# LPS- Total Groom
+# Just group observation data: Mixed model with group and lps bias as fixed effects
+print('Group')
+glmernb_group_model <- glmer.nb(TotalGroomIDInterruptedDur ~ Group*CEmotionBiasOnscreenAmbigOther.lps+Cscore_m+(1|Subject), data = all_data_filt, control = glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e6)))
+
+print(glmernb_group_model)
+
+glmernb_results<-Anova(glmernb_group_model, type = 3)
+print(glmernb_results)
+
+print(emmeans(glmernb_group_model,pairwise~Group, type='response'))
+print(emtrends(glmernb_group_model,~Group,var="Cscore_m", type='response'))
+
+pw<-emtrends(glmernb_group_model,~Group|Group,var="CEmotionBiasOnscreenAmbigOther.lps")
+print(summary(pw, infer = c(TRUE, TRUE)))
+
+summary_data_filt<-summaryBy(EmotionBiasOnscreenAmbigOther.lps+TotalGroomIDInterruptedDur~Subject+Group, data = all_data_filt, keep.names = TRUE)
+group_pred<-plot_fit(glmernb_group_model,focal_var = "CEmotionBiasOnscreenAmbigOther.lps",inter_var = c("Group"),RE = "Subject")
+group_pred$CEmotionBiasOnscreenAmbigOther.lps<-(group_pred$CEmotionBiasOnscreenAmbigOther.lps*sd(wide_summary_gaze_data$EmotionBiasOnscreenAmbigOther.lps))+mean(wide_summary_gaze_data$EmotionBiasOnscreenAmbigOther.lps)
+
+dev.new()
+g <- ggplot(all_data_filt)+
+ geom_point(aes(x=EmotionBiasOnscreenAmbigOther.lps, y=TotalGroomIDInterruptedDur, colour=Group), alpha = 1, position='jitter') +
+ geom_point(data=summary_data_filt, aes(x=EmotionBiasOnscreenAmbigOther.lps, y=TotalGroomIDInterruptedDur, colour=Group), size=3)+
+ geom_line(aes(x=CEmotionBiasOnscreenAmbigOther.lps, y=Pred, colour=Group), data=group_pred, size=1) +
+ geom_ribbon(aes(x=CEmotionBiasOnscreenAmbigOther.lps,y=Pred, fill=Group,ymin=LSE,ymax=USE, colour=Group), data=group_pred, alpha=1)+
+ scale_colour_brewer(palette='Set1')+theme_bw()
+print(g)
+
+
+
+
+
+## GROOM FREQUENCY
+# Remove outliers - within subject
+obs_sds<-2.5
+print(paste0('Removing outliers >',obs_sds,' deviations from the mean'))
+
+
+#### Total Groom
+# Remove outliers from behavioural data
+obs_data_filt<-data.frame()
+subjects<-unique(obs_data$Subject)
+for (i in 1:length(subjects)) {
+  subj_data<-obs_data[obs_data$Subject==subjects[i],]
+  mean_val<-mean(subj_data$TotalGroomIDInterruptedFreq)
+  sd_val<-sd(subj_data$TotalGroomIDInterruptedFreq)
+  outliers<-which(abs(subj_data$TotalGroomIDInterruptedFreq)>mean_val+obs_sds*sd_val)
+  if (length(outliers)==0) {
+    subj_data_filt<-subj_data
+  } else{
+    subj_data_filt<-subj_data[-outliers, ]
+  }
+  obs_data_filt<-rbind(obs_data_filt, subj_data_filt)
+}
+noutliers<-nrow(obs_data)-nrow(obs_data_filt)
+print('')
+print(paste0('Within subject: ',noutliers,' removed from behavior'))
+
+
+# Merge behavior and gaze data
+all_data_filt<-merge(wide_summary_gaze_data,obs_data_filt,by=c('Subject'),all.x = TRUE, all.y = TRUE)
+all_data_filt<-merge(all_data_filt,ranks,by=c('Subject'),all.x = TRUE, all.y = TRUE)
+all_data_filt$Subject<-as.factor(all_data_filt$Subject)
+all_data_filt<-merge(all_data_filt,sex_data,by=c('Subject'))
+
+
+
+# THREAT- Total Groom
+# Just group observation data: Mixed model with group and threat bias as fixed effects
+print('Group')
+glmernb_group_model <- glmer.nb(TotalGroomIDInterruptedFreq ~ Group*CEmotionBiasOnscreenAmbigOther.threat+Cscore_m+(1|Subject), data = all_data_filt, control = glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e6)))
+print(glmernb_group_model)
+
+glmernb_results<-Anova(glmernb_group_model, type = 3)
+print(glmernb_results)
+
+print(emmeans(glmernb_group_model,pairwise~Group, type='response'))
+print(emtrends(glmernb_group_model,~Group,var="Cscore_m", type='response'))
+pw<-emtrends(glmernb_group_model,~Group|Group,var="CEmotionBiasOnscreenAmbigOther.threat")
+print(summary(pw, infer = c(TRUE, TRUE)))
+
+summary_data_filt<-summaryBy(EmotionBiasOnscreenAmbigOther.threat+TotalGroomIDInterruptedFreq~Subject+Group, data = all_data_filt, keep.names = TRUE)
+group_pred<-plot_fit(glmernb_group_model,focal_var = "CEmotionBiasOnscreenAmbigOther.threat",inter_var = c("Group"),RE = "Subject")
+group_pred$CEmotionBiasOnscreenAmbigOther.threat<-(group_pred$CEmotionBiasOnscreenAmbigOther.threat*sd(wide_summary_gaze_data$EmotionBiasOnscreenAmbigOther.threat))+mean(wide_summary_gaze_data$EmotionBiasOnscreenAmbigOther.threat)
+
+g <- ggplot(all_data_filt)+
+ geom_point(aes(x=EmotionBiasOnscreenAmbigOther.threat, y=TotalGroomIDInterruptedFreq, colour=Group), alpha = 0.2, position='jitter') +
+ geom_point(data=summary_data_filt, aes(x=EmotionBiasOnscreenAmbigOther.threat, y=TotalGroomIDInterruptedFreq, colour=Group), size=3)+
+ geom_line(aes(x=CEmotionBiasOnscreenAmbigOther.threat, y=Pred, colour=Group), data=group_pred, size=1) +
+ geom_ribbon(aes(x=CEmotionBiasOnscreenAmbigOther.threat,y=Pred, fill=Group,ymin=LSE,ymax=USE, colour=Group), data=group_pred, alpha=0.5)+
+ scale_colour_brewer(palette='Set1')+theme_bw()
+print(g)
+
+
+
+#LPS- Total Groom
+# Just group observation data: Mixed model with group and lps bias as fixed effects
+print('Group')
+glmernb_group_model <- glmer.nb(TotalGroomIDInterruptedFreq ~ Group*CEmotionBiasOnscreenAmbigOther.lps+Cscore_m+(1|Subject), data = all_data_filt, control = glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e6)))
+print(glmernb_group_model)
+
+glmernb_results<-Anova(glmernb_group_model, type = 3)
+print(glmernb_results)
+
+print(emmeans(glmernb_group_model,pairwise~Group, type='response'))
+print(emtrends(glmernb_group_model,~Group,var="Cscore_m", type='response'))
+pw<-emtrends(glmernb_group_model,~Group|Group,var="CEmotionBiasOnscreenAmbigOther.lps")
+print(summary(pw, infer = c(TRUE, TRUE)))
+
+
+summary_data_filt<-summaryBy(EmotionBiasOnscreenAmbigOther.lps+TotalGroomIDInterruptedFreq~Subject+Group, data = all_data_filt, keep.names = TRUE)
+group_pred<-plot_fit(glmernb_group_model,focal_var = "CEmotionBiasOnscreenAmbigOther.lps",inter_var = c("Group"),RE = "Subject")
+group_pred$CEmotionBiasOnscreenAmbigOther.lps<-(group_pred$CEmotionBiasOnscreenAmbigOther.lps*sd(wide_summary_gaze_data$EmotionBiasOnscreenAmbigOther.lps))+mean(wide_summary_gaze_data$EmotionBiasOnscreenAmbigOther.lps)
+
+g <- ggplot(all_data_filt)+
+ geom_point(aes(x=EmotionBiasOnscreenAmbigOther.lps, y=TotalGroomIDInterruptedFreq, colour=Group), alpha = 1, position='jitter') +
+ geom_point(data=summary_data_filt, aes(x=EmotionBiasOnscreenAmbigOther.lps, y=TotalGroomIDInterruptedFreq, colour=Group), size=3)+
+ geom_line(aes(x=CEmotionBiasOnscreenAmbigOther.lps, y=Pred, colour=Group), data=group_pred, size=1) +
+ geom_ribbon(aes(x=CEmotionBiasOnscreenAmbigOther.lps,y=Pred, fill=Group,ymin=LSE,ymax=USE, colour=Group), data=group_pred, alpha=1)+
+ scale_colour_brewer(palette='Set1')+theme_bw()
+print(g)
